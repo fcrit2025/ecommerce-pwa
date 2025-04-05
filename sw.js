b1a2e4f36663d57e222c6c17d1c99106655862cc
fcrit2025 <pythonfh421@gmail.com>
2025-04-05 06:48:11 +0000

    sw.js updated

--- sw.js
+++ sw.js
@@ -7,15 +7,22 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
+// Install event
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', event => {
+// Fetch event
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
